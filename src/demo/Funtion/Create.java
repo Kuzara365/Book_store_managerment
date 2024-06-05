8b0699088c1a0db748f824ece66d3beb437e0ff8
Kuzar365 <vothanhphuc365@gmail.com>
2024-06-06 04:23:07 +0700

    Minor fix

--- src/demo/Funtion/Create.java
+++ src/demo/Funtion/Create.java
@@ -1,7 +1,6 @@
 package demo.Funtion;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
--- src/demo/controller/AccountStorage.java
+++ src/demo/controller/AccountStorage.java
@@ -1,6 +1,5 @@
 package demo.controller;
 
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
